3b2b1c5ac3b00fe0c916c58c833cbe755ac4aedb
mahek-batra <mahekbatra@ufl.edu>
2024-11-15 21:19:13 -0500

    added router statements in main.py

--- SERVER/app/main.py
+++ SERVER/app/main.py
@@ -17,3 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(api_router, dependencies=[Depends(get_db)])
+
+api_router.include_router(questions.router, prefix="/questions", tags=["questions"])
+
+api_router.include_router(answers.router, prefix="/answers", tags=["answers"])
