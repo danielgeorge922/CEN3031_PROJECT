83d943d418bea86bc0641ddc385425a3bf2b9482
alexruah <alex.ruah7@gmail.com>
2024-11-25 11:39:56 -0500

    added likes to db

--- SERVER/app/db/models.py
+++ SERVER/app/db/models.py
@@ -21,7 +21,6 @@ class User(Base):
     classes = relationship("Class", secondary=user_classes, back_populates="users")
     questions = relationship("Question", back_populates="user")
     answers = relationship("Answer", back_populates="user")
-    likes = relationship("Like", back_populates="user")
 
 class Class(Base):
     __tablename__ = 'classes'
@@ -51,18 +50,8 @@ class Answer(Base):
     content = Column(String, nullable=False)
     question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    likes = Column(Integer, default=0)
+    dislikes = Column(Integer, default=0)
 
     user = relationship("User", back_populates="answers")
     question = relationship("Question", back_populates="answers")
-    likes = relationship("Like", back_populates="answer")
-    
-
-class Like(Base):
-    __tablename__ = 'likes'
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    answer_id = Column(Integer, ForeignKey('answers.id'), nullable=False)
-    dislike = Column(Boolean, default=False)
-
-    user = relationship("User")
-    answer = relationship("Answer")
